2f334e5b03e42acdf1438283852971aa885d81b8
al2718x <al2718x@gmail.com>
2024-08-05 17:11:19 +0300

    add managing translation pair in popup

--- popup.js
+++ popup.js
@@ -6,15 +6,43 @@ function hashCode(s) {
 }
 
 function refresh() {
+    let iTranslateFrom = document.getElementById('i-translate-from');
+    let iTranslateTo = document.getElementById('i-translate-to');
+    let iSetupSave = document.getElementById('i-setup-save');
     let iPairs = document.getElementById('i-pairs');
     let iExport = document.getElementById('i-export');
+    browser.storage.local.get('config').then((data) => {
+        let config = data['config'];
+        if (!config) {
+            config = {};
+        }
+        if (!config['translate-from'] || !config['translate-to']) {
+            config['translate-from'] = 'en';
+            config['translate-to'] = 'it';
+            browser.storage.local.set({ 'config': config });
+        }
+        iTranslateFrom.value = config['translate-from'] ?? 'en';
+        iTranslateTo.value = config['translate-to'] ?? 'it';
+    }, (error) => console.log(error));
+    iSetupSave.addEventListener('click', () => {
+        browser.storage.local.get('config').then((data) => {
+            let config = data['config'];
+            if (!config) {
+                config = {};
+            }
+            config['translate-from'] = iTranslateFrom.value;
+            config['translate-to'] = iTranslateTo.value;
+            browser.storage.local.set({ 'config': config });
+        }, (error) => console.log(error));
+    });
     let pairs = new Map();
     let gettingAllStorageItems = browser.storage.local.get(null);
     gettingAllStorageItems.then((data) => {
         let keys = Object.keys(data);
+        // console.log(keys);
         for (let key of keys) {
             let value = data[key];
-            console.log(key + ' ' + value);
+            // console.log(key + ' ' + value);
             pairs.set(key, value);
         }
         let pairsSorted = new Map([...pairs.entries()].sort());
