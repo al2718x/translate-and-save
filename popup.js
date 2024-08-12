ea388cf2da5ea4b4208be84e159ce2b0b8ed0f47
al2718x <al2718x@gmail.com>
2024-08-12 23:18:25 +0300

    popup text translate in popup form, refactor

--- popup.js
+++ popup.js
@@ -5,31 +5,94 @@ function hashCode(s) {
     );
 }
 
+function transSave(item) {
+    item.style.cursor = 'pointer';
+    item.title = 'Save';
+    item.addEventListener('click', async function () {
+        let storage_data = await browser.storage.local.get(null);
+        let translation = storage_data['translation'] ?? {};
+        let trans_src = document.getElementById('i-translate-src').value.trim().toLowerCase();
+        let trans_res = document.getElementById(item.dataset.trans_id).innerText;
+        translation[trans_src] = trans_res;
+        await browser.storage.local.set({ 'translation': translation });
+        refresh();
+    });
+}
+
+function transDelete(item) {
+    item.style.fontSize = '10px';
+    item.style.cursor = 'pointer';
+    item.title = 'Delete';
+    item.addEventListener('click', async function () {
+        let storage_data = await browser.storage.local.get(null);
+        let translation = storage_data['translation'] ?? {};
+        let trans = document.getElementById(item.dataset.trans_id).innerText;
+        delete translation[trans];
+        await browser.storage.local.set({ 'translation': translation });
+        refresh();
+    });
+}
+
+async function translate() {
+    let iTranslateSrc = document.getElementById('i-translate-src');
+    let iTranslateRes = document.getElementById('i-translate-res');
+    let trans_src = iTranslateSrc.value.trim().toLowerCase();
+    console.log('TRANSLATE SOURCE: ' + trans_src);
+    if ('' === trans_src) return;
+
+    let storage_data = await browser.storage.local.get(null);
+    let trans_from = storage_data['config']['translate-from'] ?? 'en';
+    let trans_to = storage_data['config']['translate-to'] ?? 'it';
+    let request = `https://api.mymemory.translated.net/get?langpair=${trans_from}|${trans_to}&q=${trans_src}`;
+
+    fetch(request).then(function (response) {
+        return response.json();
+    }).then(function (data) {
+        let trans_res = data.responseData.translatedText.toLowerCase();
+        let trans_res_all = [trans_res];
+        for (m of data.matches) {
+            let tmp = m.translation.trim().toLowerCase();
+            if ('' === tmp) continue;
+            if (trans_res_all.includes(tmp)) continue;
+            trans_res_all.push(tmp);
+        }
+        console.log('TRANSLATE RESULT: ' + trans_res_all);
+        iTranslateRes.innerHTML = trans_res_all
+            .map((item) => {
+                let id = 'trans-save' + hashCode(item);
+                return `
+                <span class="trans-save" data-trans_id="${id}">💾</span>
+                <span id="${id}">${item}</span>
+                `;
+            })
+            .join('<span style="display:block;height:5px;"></span>');
+
+        document.querySelectorAll('.trans-save').forEach((item) => transSave(item));
+    }).catch(function (err) {
+        console.log('TRANSLATE FETCH ERROR: ', err);
+    });
+}
+
+async function getSelectedText() {
+    let iTranslateSrc = document.getElementById('i-translate-src');
+    try {
+        let tab = (await browser.tabs.query({ currentWindow: true, active: true }))[0];
+        let selectedText = await browser.tabs.sendMessage(tab.id, { message: 'getSelectedText' });
+        iTranslateSrc.value = selectedText;
+    } catch (e) {
+        console.log('GET SELECTED TEXT ERROR: ', e);
+    }
+}
+
 async function refresh() {
     let iTranslate = document.getElementById('i-translate');
-    let iTranslateSrc = document.getElementById('i-translate-src');
     let iTranslateFrom = document.getElementById('i-translate-from');
     let iTranslateTo = document.getElementById('i-translate-to');
     let iSetupSave = document.getElementById('i-setup-save');
     let iPairs = document.getElementById('i-pairs');
     let iExport = document.getElementById('i-export');
 
-    iTranslate.addEventListener('click', async function () {
-        try {
-            let tab = (await browser.tabs.query({ currentWindow: true, active: true }))[0];
-            await browser.tabs.sendMessage(tab.id, { message: 'translateText', text: iTranslateSrc.value });
-        } catch (e) {
-            console.log('POPUP ERROR: ', e);
-        }
-    });
-
-    try {
-        let tab = (await browser.tabs.query({ currentWindow: true, active: true }))[0];
-        let selectedText = await browser.tabs.sendMessage(tab.id, { message: 'getSelectedText' });
-        iTranslateSrc.value = selectedText;
-    } catch (e) {
-        console.log('POPUP ERROR: ', e);
-    }
+    iTranslate.addEventListener('click', () => translate());
 
     let data = await browser.storage.local.get(null);
     let config = data['config'] ?? {};
@@ -59,7 +122,7 @@ async function refresh() {
     iPairs.innerHTML = '';
     iExport.innerHTML = '';
     pairsSorted.forEach((value, key, map) => {
-        let id = 'trans-' + hashCode(key);
+        let id = 'trans-delete' + hashCode(key);
         iPairs.innerHTML += `
             <div>
             <span class="trans-delete" data-trans_id="${id}">❌</span>
@@ -70,17 +133,11 @@ async function refresh() {
         iExport.innerHTML += `${key} = ${value}\r\n`;
     });
 
-    document.querySelectorAll('.trans-delete').forEach(function (item) {
-        item.style.fontSize = '10px';
-        item.style.cursor = 'pointer';
-        item.title = 'Delete';
-        item.addEventListener('click', async function () {
-            let trans = document.getElementById(item.dataset.trans_id).innerText;
-            delete translation[trans];
-            await browser.storage.local.set({ 'translation': translation });
-            refresh();
-        });
-    });
+    document.querySelectorAll('.trans-delete').forEach((item) => transDelete(item));
 }
 
-refresh();
+(async function run() {
+    await getSelectedText();
+    await translate();
+    await refresh();
+})();
