c320eb9e3e2afbadf4c4a6d5d37802d6e94d423f
al2718x <al2718x@gmail.com>
2024-08-13 23:52:04 +0300

    improve popup ui

--- popup.js
+++ popup.js
@@ -85,14 +85,14 @@ async function getSelectedText() {
 }
 
 async function refresh() {
-    let iTranslate = document.getElementById('i-translate');
+    let btnTranslate = document.getElementById('btn-translate');
     let iTranslateFrom = document.getElementById('i-translate-from');
     let iTranslateTo = document.getElementById('i-translate-to');
-    let iSetupSave = document.getElementById('i-setup-save');
+    let btnSetupSave = document.getElementById('btn-setup-save');
     let iPairs = document.getElementById('i-pairs');
     let iExport = document.getElementById('i-export');
 
-    iTranslate.addEventListener('click', () => translate());
+    btnTranslate.addEventListener('click', () => translate());
 
     let data = await browser.storage.local.get(null);
     let config = data['config'] ?? {};
@@ -103,7 +103,7 @@ async function refresh() {
     }
     iTranslateFrom.value = config['translate-from'] ?? 'en';
     iTranslateTo.value = config['translate-to'] ?? 'it';
-    iSetupSave.addEventListener('click', async function () {
+    btnSetupSave.addEventListener('click', async function () {
         let data = await browser.storage.local.get(null);
         let config = data['config'] ?? {};
         config['translate-from'] = iTranslateFrom.value;
