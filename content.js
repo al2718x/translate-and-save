c476c2d1684fd3f808129702a715b293eb378867
al2718x <al2718x@gmail.com>
2024-08-20 22:21:50 +0300

    improve picking and translating selected text

--- content.js
+++ content.js
@@ -1,11 +1,24 @@
+// document.addEventListener('mouseup', async (e) => {
+//     if (e.ctrlKey) {
+//         console.log(getSelectedText());
+//     }
+// });
+
 browser.runtime.onMessage.addListener((request) => {
     if ('getSelectedText' == request.message) {
         return new Promise(resolve => {
-            setTimeout(() => resolve(document.getSelection().toString().toLowerCase()), 100);
+            setTimeout(() => resolve(getSelectedText().toLowerCase()), 100);
         });
     }
-    
+
     return new Promise(resolve => {
         setTimeout(() => resolve(''), 100);
     });
 });
+
+function getSelectedText() {
+    let element = document.activeElement;
+    return 'INPUT' === element.tagName || 'TEXTAREA' === element.tagName
+        ? element.value.substring(element.selectionStart, element.selectionEnd)
+        : window.getSelection()?.toString() ?? '';
+}
