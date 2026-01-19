9aedc7564519b465c576e82e48f9a49f19cbb18f
al2718x <al2718x@gmail.com>
2026-01-19 12:15:55 +0300

    fix hang on empty selection

--- content.js
+++ content.js
@@ -7,21 +7,29 @@
 browser.runtime.onMessage.addListener((request) => {
     if ('getSelectedText' == request.message) {
         return new Promise(resolve => {
-            setTimeout(() => resolve(getSelectedText().toLowerCase()), 100);
+            setTimeout(() => resolve(getSelectedText().trim().toLowerCase()), 10);
         });
     }
     return new Promise(resolve => {
-        setTimeout(() => resolve(''), 100);
+        setTimeout(() => resolve(''), 10);
     });
 });
 
 function getSelectedText() {
-    let selection = window.getSelection();
-    if (selection && selection.rangeCount) {
-        return selection.getRangeAt(0).toString();
+    if (document.readyState !== 'complete') {
+        return '';
+    }
+    let selection = null;
+    for (let i = 0; i < 100; i++) {
+        selection = window.getSelection();
+        if (selection && selection.rangeCount) {
+            return selection.getRangeAt(0).toString();
+        }
+        setTimeout(() => { }, 1);
     }
     let element = document.activeElement;
     if (element && ('INPUT' === element.tagName || 'TEXTAREA' === element.tagName)) {
         return element.value.substring(element.selectionStart, element.selectionEnd);
     }
+    throw new Error('No selection');
 }
