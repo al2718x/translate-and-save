c45727b3a676e4ddf0894216fa14022886f96b9e
al2718x <al2718x@gmail.com>
2026-02-02 12:39:09 +0300

    clear selected text before translate

--- popup.js
+++ popup.js
@@ -459,7 +459,7 @@
             let tabs = await browser.tabs.query({ currentWindow: true, active: true });
             let tab = tabs[0];
             let selectedText = await browser.tabs.sendMessage(tab.id, { message: 'getSelectedText' });
-            iTranslateSrc.value = selectedText;
+            iTranslateSrc.value = selectedText.replace(/\s+\n/g, '\n').replace(/\n\s+/g, '\n');
         } catch (e) {
             console.log('GET SELECTED TEXT ERROR: ', e);
         }
